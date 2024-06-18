318902bd08201df618da57b8acc1e3f1376780cc
Mukul Anand Bhatt <onlyforwork941@gmail.com>
2024-06-19 01:28:06 +0530

    Completed User functionalities and Auth

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const User = require("../models/user");
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const {authenticateToken} = require("./userAuth")
 // signup
 router.post("/signup", async (req, res) => {
-    console.log(req.body)
+    // console.log(req.body)
     try {
         const { username, email, password, address } = req.body;
 
@@ -27,18 +29,16 @@ router.post("/signup", async (req, res) => {
         if (password.length <= 5) {
             return res.status(400).json({ message: "Password length should be greater than 5" });
         }
+        const hashPass = await bcrypt.hash(password,10);
 
         // Create new user
         const newUser = new User({
             username: username,
             email: email,
-            password: password,
+            password: hashPass,
             address: address
         });
-        console.log(username)
-        console.log(password)
-        console.log(email)
-        console.log(address)
+        
         await newUser.save();
 
         return res.status(200).json({ message: "Signup Successful!!" });
@@ -48,5 +48,58 @@ router.post("/signup", async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 });
+
+//Sign in 
+router.post("/signin", async (req, res) => {
+    try{
+        const {username, password} = req.body
+        const user = await User.findOne({username: username})
+        if (!user){
+            return res.status(400).json({message: "User not found"})
+            };
+        const validPass = await bcrypt.compare(password, user.password)
+        if (validPass){
+            const authClaims =[
+                {name:user.username},
+                {role: user.role}
+            ]
+            const token = jwt.sign({authClaims},"bookstore123",{
+                expiresIn:"30d"
+            });
+            return res.status(200).json({id: user._id, role: user.role,token: token});
+        }
+        else{
+            return res.status(400).json({message: "Invalid password"})
+        }
+    }
+    catch(error){
+        console.error("Error during signin",error);
+        res.status(500).json({message:"Internal Server error"});
+    }
+});
+
+//get user information 
+router.get("/userinfo", authenticateToken, async (req,res)=>{
+    try{
+        const {id} = req.headers;
+        const data = await User.findById(id).select('-password');
+        return res.status(200).json(data);
+    }
+    catch(error){
+        res.status(500).json({message:"Internal server error"});
+    }
+});
+
+router.put("/updateadd", authenticateToken ,async (req,res)=>{
+    try{
+        const{id} = req.headers;
+        const {address} = req.body;
+        await User.findByIdAndUpdate(id,{address:address});
+        return res.status(200).json({message:"Address Updated Successfully"})
+    }
+    catch{
+        res.status(500).json({message:"Internal Server Error"})
+    }
+})
 
 module.exports = router;
---
+++ backend/routes/userAuth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const authenticateToken = (req,res,next)=>{
+    const authHeader =req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(token == null){
+        return res.status(401).json({message:"Unauthorized"});
+    }
+    jwt.verify(token,"bookstore123",(err,user)=>{
+        if(err){
+            return res.status(403).json({message:"Token expired.please signin again"});
+        }
+        req.user = user;
+        next();
+    });
+};
+
+module.exports={authenticateToken};
