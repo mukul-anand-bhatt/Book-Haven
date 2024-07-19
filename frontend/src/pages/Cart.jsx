334cb1d05322f2d7d81de1111d07df42aa3137ba
Mukul Anand Bhatt <onlyforwork941@gmail.com>
2024-07-19 18:49:38 +0530

    started off with cart

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
-export default function app(){
-    return (
-        <div>
-            Cart
-        </div>
-    )
-}
+const cart = () => {
+    const [Cart , setCart] = useState()
+    return <>
+    {!Cart && <Loader/>}
+        {Cart && Cart.length===0 && (
+            <div className="h-screen">
+                <div className="h-[100%] flex items-center justify-center flex-col">
+                    <h1 className="text-5xl lg:text-6xl font-semibold text-zinc-400">
+                        Empty Cart
+                    </h1>
+                    <img/>
+                </div>
+            </div>
+        )}
+    </>;
+};
+
+export default cart;
